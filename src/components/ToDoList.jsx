f8737205435e3fc2eb8ee043543153571065b9a2
Prasanna Shrestha <sthaprasanna131@gmail.com>
2025-01-13 12:24:32 +0545

    Updated UI of the system.

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -69,147 +69,62 @@ const ToDoList = () => {
   return (
     <div className={`w-screen h-screen ${lightTheme ? "bg-[#f9fafb]" : "bg-[#1e1e1e]"}`}>
       <div className="flex justify-end p-4">
-        <button
-          className={`px-4 py-2 rounded-md ${
-            lightTheme ? "bg-[#e0e7ff] text-[#1e3a8a]" : "bg-[#333] text-white"
-          }`}
-          onClick={changeTheme}
-        >
+        <button className={`px-4 py-2 rounded-md ${lightTheme ? "bg-[#e0e7ff] text-[#1e3a8a]" : "bg-[#333] text-white"}`} onClick={changeTheme}>
           {lightTheme ? "Dark Theme" : "Light Theme"}
         </button>
       </div>
 
-      <div
-        className="w-full sm:w-[50vw] mx-auto p-6 rounded-lg"
-        style={{
-          boxShadow: lightTheme
-            ? "0 4px 10px rgba(0, 0, 0, 0.1)"
-            : "0 4px 10px rgba(0, 0, 0, 0.4)",
-          background: lightTheme ? "#f5f5f5" : "linear-gradient(135deg, #434343, #1f1f1f)", // Soft greyish white
-        }}
-      >
+      <div className="w-full sm:w-[50vw] mx-auto p-6 rounded-lg" style={{ boxShadow: lightTheme ? "0 4px 10px rgba(1px, 1px, 10px, 10px, 0.1)" : "0 4px 10px rgba(0, 0, 0, 0.4)", background: lightTheme ? "#f5f5f5" : "#1f1f1f" }}>
         <div className="flex justify-between items-center">
-          <h1
-            className={`text-2xl font-semibold ${
-              lightTheme ? "text-[#1e3a8a]" : "text-white"
-            }`}
-          >
-            To-Do List
-          </h1>
-          <button
-            className={`px-4 py-2 rounded-md ${
-              lightTheme ? "bg-[#e0e7ff] text-[#1e3a8a]" : "bg-[#4caf50] text-white"
-            }`}
-            onClick={displayForm}
-          >
-            + Add Task
-          </button>
+          <h1 className={`text-2xl font-semibold ${lightTheme ? "text-[#1e3a8a]" : "text-white"}`}>To-Do List</h1>
+          <button className={`px-4 py-2 rounded-md ${lightTheme ? "bg-[#e0e7ff] text-[#1e3a8a]" : "bg-[#4caf50] text-white"}`} onClick={displayForm}>+ Add Task</button>
         </div>
 
         {showInput && (
           <div className="mt-4">
-            <input
-              type="text"
-              placeholder="Task Title"
-              className="w-full p-2 border rounded-md mb-3 outline-none text-black"
-              value={content}
-              onChange={handleTitleChange}
-            />
-            <textarea
-              placeholder="Task Description"
-              className="w-full p-2 border rounded-md mb-3 outline-none text-black"
-              value={description}
-              onChange={handleDescriptionChange}
-            ></textarea>
-            <button
-              className="px-4 py-2 bg-[#4caf50] text-white rounded-md"
-              onClick={setTask}
-            >
-              Save Task
-            </button>
+            <input type="text" placeholder="Task Title" className={`w-full p-2 border rounded-md mb-3 outline-none bg-transparent ${lightTheme ? "text-black" : "text-white"}`} value={content} onChange={handleTitleChange} />
+            <textarea placeholder="Task Description" className={`w-full p-2 border rounded-md mb-3 outline-none text-black bg-transparent ${lightTheme ? "text-black" : "text-white"}`} value={description} onChange={handleDescriptionChange}></textarea>
+            <button className="px-4 py-2 bg-[#4caf50] text-white rounded-md bg-tra" onClick={setTask}>Save Task</button>
           </div>
         )}
 
         <div className="mt-6">
-          <h2
-            className={`text-lg font-medium ${
-              lightTheme ? "text-gray-800" : "text-white"
-            }`}
-          >
-            Pending Tasks
-          </h2>
+          <h2 className={`text-lg font-medium ${lightTheme ? "text-gray-800" : "text-white"}`}>Pending Tasks</h2>
           {pendingTasks.map((task, index) => (
-            <div
-              key={index}
-              className="mt-4 flex justify-between items-center border-b pb-2"
-            >
+            <div key={index} className="mt-4 flex justify-between items-center border-b pb-2">
               <div>
-                <div
-                  className={`font-semibold cursor-pointer ${
-                    task.completed ? "line-through text-gray-500" : ""
-                  } hover:underline`}
-                  onClick={() => toggleDescription(index)}
-                >
-                  {task.content}
-                </div>
+                <div className={`font-semibold cursor-pointer ${task.completed ?? "line-through"} ${lightTheme ? "text-gray-700" : "text-gray-300"} hover:underline`} onClick={() => toggleDescription(index)}>{task.content}</div>
                 {task.show && (
-                  <p
-                    className={`mt-1 ${
-                      lightTheme ? "text-gray-700" : "text-gray-300"
-                    }`}
-                  >
+                  <p className={`mt-1 ${lightTheme ? "text-gray-700" : "text-gray-300"}`}>
                     {task.description}
                   </p>
                 )}
               </div>
               <div className="flex gap-2">
-                <button
-                  className="px-4 py-2 bg-[#4caf50] text-white rounded-md"
-                  onClick={() => markAsCompleted(index)}
-                >
+                <button className="px-4 py-2 bg-[#4caf50] text-white rounded-md" onClick={() => markAsCompleted(index)}>
                   {task.completed ? "Undo" : "Completed"}
                 </button>
-                <button
-                  className="px-4 py-2 bg-red-500 text-white rounded-md"
-                  onClick={() => deleteTask(index)}
-                >
-                  Delete
-                </button>
+                <button className="px-4 py-2 bg-red-500 text-white rounded-md" onClick={() => deleteTask(index)}>Delete</button>
               </div>
             </div>
           ))}
         </div>
 
-        <button
-          className="mt-6 px-4 py-2 bg-[#007bff] text-white rounded-md"
-          onClick={() => setShowCompleted(!showCompleted)}
-        >
+        <button className="mt-6 px-4 py-2 bg-[#007bff] text-white rounded-md" onClick={() => setShowCompleted(!showCompleted)}>
           {showCompleted ? "Hide Completed Tasks" : "Show Completed Tasks"}
         </button>
 
         {showCompleted && completedTasks.length > 0 && (
           <div className="mt-6">
-            <h2
-              className={`text-lg font-medium ${
-                lightTheme ? "text-gray-800" : "text-white"
-              }`}
-            >
+            <h2 className={`text-lg font-medium ${lightTheme ? "text-gray-800" : "text-white"}`}>
               Completed Tasks
             </h2>
             {completedTasks.map((task, index) => (
-              <div
-                key={index}
-                className="mt-4 flex justify-between items-center border-b pb-2"
-              >
-                <div className="font-semibold text-gray-500">
+              <div key={index} className="mt-4 flex justify-between items-center border-b pb-2">
+                <div className={`font-semibold ${lightTheme ? "text-gray-700" : "text-gray-300"}`}>
                   {task.content}
                 </div>
-                <button
-                  className="px-4 py-2 bg-[#4caf50] text-white rounded-md"
-                  onClick={() => markAsCompleted(index)}
-                >
-                  Undo
-                </button>
+                <button className="px-4 py-2 bg-[#4caf50] text-white rounded-md" onClick={() => markAsCompleted(index)}>Undo</button>
               </div>
             ))}
           </div>
